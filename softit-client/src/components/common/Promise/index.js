41a09c50518d471b76bea13e05c6de1d06660b07
yongtany <yongn1494@gmail.com>
2019-03-21 14:48:53 +0900

    Services section

---
+++ softit-client/src/components/common/Promise/index.js
@@ -0,0 +1,3 @@
+import Promise from './Promise';
+
+export default Promise;
--- softit-client/src/pages/LandingPage.js
+++ softit-client/src/pages/LandingPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from 'components/common/Banner';
 import Services from 'components/common/Services';
+import Promise from 'components/common/Promise';
 
 const LandingPage = () => {
   return (
     <div>
       <Banner />
       <Services />
+      <Promise />
     </div>
   );
 };
