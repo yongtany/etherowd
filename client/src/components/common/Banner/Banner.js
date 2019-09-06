a6a54738128fd40022c1e794897e2be9dc55691c
yongtany <yongn1494@gmail.com>
2019-09-06 22:42:57 +0900

    [CREATE] Login with Metamask at Front-end

--- client/src/components/common/Banner/Banner.js
+++ client/src/components/common/Banner/Banner.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import WOW from 'wow.js';
 import styles from './Banner.scss';
 import classNames from 'classnames/bind';
+import web3 from 'ethereum/web3';
 
 const cx = classNames.bind(styles);
 
@@ -10,6 +11,73 @@ class Banner extends Component  {
   componentDidMount() {
     new WOW().init();
   }
+
+  handleClick = async () => {
+    const { onLoggedIn } = this.props;
+
+    // Check if MetaMask is installed
+    if (!window.ethereum) {
+      window.alert('Please install MetaMask first.');
+      return;
+    }
+
+    const coinbase = await web3.eth.getCoinbase();
+    if (!coinbase) {
+      window.alert('Please activate MetaMask first.');
+      return;
+    }
+
+    const publicAddress = coinbase.toLowerCase();
+    this.setState({ loading: true });
+
+    // Look if user with current publicAddress is already present on backend
+    fetch(
+      `${
+        process.env.REACT_APP_BACKEND_URL
+      }/users?publicAddress=${publicAddress}`
+    )
+      .then(response => response.json())
+      // If yes, retrieve it. If no, create it.
+      .then(users =>
+        users.length ? users[0] : this.handleSignup(publicAddress)
+      )
+      // Popup MetaMask confirmation modal to sign message
+      .then(this.handleSignMessage)
+      // Send signature to backend on the /auth route
+      .then(this.handleAuthenticate)
+      // Pass accessToken back to parent component (to save it in localStorage)
+      .then(onLoggedIn)
+      .catch(err => {
+        window.alert(err);
+        this.setState({ loading: false });
+      });
+  };
+
+  handleSignMessage = async ({
+    publicAddress,
+    nonce
+  }) => {
+    try {
+      const signature = await web3.eth.personal.sign(
+        `I am signing my one-time nonce: ${nonce}`,
+        publicAddress,
+        '' // MetaMask will ignore the password argument here
+      );
+      return { publicAddress, signature };
+    } catch (err) {
+      throw new Error('You need to sign the message to be able to log in.');
+    }
+  };
+
+  handleSignup = publicAddress => {
+    return fetch(`${process.env.REACT_APP_BACKEND_URL}/users`, {
+      body: JSON.stringify({ publicAddress }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    }).then(response => response.json());
+  };
 
   render() {
     return (
@@ -19,7 +87,7 @@ class Banner extends Component  {
             <h1><strong>신뢰적 거래를 위한 이더리움 기반 크라우드 펀딩</strong><p>Etherowd</p></h1>
 
             <div className={cx("banner-underline")}></div>
-            <Link to="#signin" className={cx('link')}>시작하기</Link>
+            <div className={cx('link')} onClick={this.handleClick}>시작하기</div>
             <Link to="#signin" className={cx('link')}>자세히보기</Link>
           </div>
           <div className={cx("banner-icon")}>
